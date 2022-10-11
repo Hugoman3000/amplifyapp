c4613e37b5110e81209ae9acb9cd4174942fe47f
Hugoman3000 <steve@skasus-silver>
2022-10-10 21:03:20 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// const myFirstElement = <h1>Hello React!</h1>
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myFirstElement);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
